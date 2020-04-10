d49a2a888db78b68e095a60d14d475e628aa8017
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-04-10 17:27:50 +0200

    Add env variabels for production build - and prettier

--- src/socket.ts
+++ src/socket.ts
@@ -1,12 +1,24 @@
 import io from "socket.io-client";
 
+const getEndpointUrl = () => {
+  const secureProtocol = process.env.REACT_APP_WEBSOCKET_SECURE === "true";
+  const portString = process.env.REACT_APP_WEBSOCKET_PORT;
+
+  const protocol = secureProtocol ? "wss" : "ws";
+  const port = portString ?? window.location.port;
+
+  return `${protocol}:${window.location.hostname}:${port}`;
+};
+
+const endpoint = getEndpointUrl();
+
 export const connectSocket = () => {
-  const socket = io(`ws://${window.location.hostname}:1234`);
+  const socket = io(endpoint);
 
   return {
     socket,
     cleanup: () => {
       socket.close();
-    }
+    },
   };
 };
