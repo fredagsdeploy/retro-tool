613d0bf093deab0fd5a7432172d241e600583918
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-04-10 14:18:35 +0200

    Add dev config

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 export const connectSocket = () => {
-  const socket = io(`ws://${window.location.hostname}/ws`);
+  const socket = io(`ws://${window.location.hostname}:1234`);
 
   return {
     socket,
