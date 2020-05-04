0759d1a6123ae06941954386ed1ae2f2b0be68ba
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-05-04 20:57:58 +0200

    Add more typings for adjective-adjective-animal

--- backend/typings.d.ts
+++ backend/typings.d.ts
@@ -1,7 +1,37 @@
 declare module "adjective-adjective-animal" {
+  export type StringFormat =
+    | "upper"
+    | "lower"
+    | "sentence"
+    | "title"
+    | "camel"
+    | "pascal"
+    | "snake"
+    | "param"
+    | "dot"
+    | "path"
+    | "constant"
+    | "swap"
+    | "ucFirst"
+    | "lcFirst";
+
   /***
    * Generates a reasonably secure and unique string with adjectives
-   * @param numberOfAdjectives - the number of adjectives prefixing the animal, default 2
+   * @param adjectives - the number of adjectives prefixing the animal, default 2
    */
-  export default function (numberOfAdjectives?: number): Promise<string>;
+  export default function (adjectives?: number): Promise<string>;
+  /***
+   * Generates a reasonably secure and unique string with adjectives
+   * @param format - the number of adjectives prefixing the animal, default 2
+   */
+  export default function (format: StringFormat): Promise<string>;
+
+  /***
+   * Generates a reasonably secure and unique string with adjectives
+   * @param params - an object containing format and/or adjectives specifying the format and number of adjectives to generate
+   */
+  export default function (params: {
+    adjectives?: number;
+    format?: StringFormat;
+  }): Promise<string>;
 }
